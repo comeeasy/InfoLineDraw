9aacbbff822526eb0466a96ba22e6388d5b1e4d0
yourname <yout@email.com>
2024-04-01 20:02:17 +0900

    Bug fix.

--- scripts/download_SD_checkpoints.sh
+++ scripts/download_SD_checkpoints.sh
@@ -24,7 +24,7 @@ LORA_MODEL_PATH="${SD_WEBUI_PATH}/models/Lora"
 # Download https://drive.google.com/file/d/1PhWkky-4mMJqqgCtr5IlcQZdvIdex2T8/view?usp=sharing
 # to "stable-sd-webui/models/StableDiffusion" using gdwon. And Path existence must be checked
 # Check and create StableDiffusion model directory if it doesn't exist
-if [ ! -d "$STABLEDIFFUSION_MODEL_PATH" ]; then
+if [ -d "$STABLEDIFFUSION_MODEL_PATH" ]; then
     echo "Downloading Stable Diffusion model..."
     gdown "https://drive.google.com/uc?id=1PhWkky-4mMJqqgCtr5IlcQZdvIdex2T8" -O "${STABLEDIFFUSION_MODEL_PATH}/dreamshaperXL_v21TurboDPMSDE.safetensors"
 else
@@ -35,7 +35,7 @@ fi
 # Download https://drive.google.com/file/d/1-0npDwTU4YSe5JlOly6Va1fuz_tjlZzh/view?usp=sharing
 # to "stable-sd-webui/models/ControlNet" using gdwon. And Path existence must be checked
 # Check and create ControlNet model directory if it doesn't exist
-if [ ! -d "$CONTROLNET_MODEL_PATH" ]; then
+if [ -d "$CONTROLNET_MODEL_PATH" ]; then
     echo "Downloading ControlNet model..."
     gdown "https://drive.google.com/uc?id=1-0npDwTU4YSe5JlOly6Va1fuz_tjlZzh" -O "${CONTROLNET_MODEL_PATH}/diffusers_xl_canny_full.safetensors"
 else
@@ -44,7 +44,7 @@ fi
 
 # Download https://drive.google.com/file/d/1Gy4PVhuDVfztdTFimMtc7hfngmL2stm2/view?usp=sharing
 # to "stable-sd-webui/models/Lora" using gdwon. And Path existence must be checked
-if [ ! -d "$LORA_MODEL_PATH" ]; then
+if [ -d "$LORA_MODEL_PATH" ]; then
     echo "Downloading Lora model..."
     gdown "https://drive.google.com/uc?id=1-0npDwTU4YSe5JlOly6Va1fuz_tjlZzh" -O "${LORA_MODEL_PATH}/Line_Art_SDXL.safetensors"
 else
